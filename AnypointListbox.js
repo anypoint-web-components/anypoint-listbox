0fe743412ac4be9488d809ddfe04f53e77a5d242
Pawel <jarrodek@gmail.com>
2019-08-04 10:08:19 -0700

    feat(a11y): updating role attributes to match WAI spec

--- AnypointListbox.js
+++ AnypointListbox.js
@@ -1,6 +1,8 @@
 import { html, css, LitElement } from 'lit-element';
 import { AnypointMenuMixin } from '@anypoint-web-components/anypoint-menu-mixin/anypoint-menu-mixin.js';
 
+let globalId = 1;
+
 export class AnypointListbox extends AnypointMenuMixin(LitElement) {
   static get styles() {
     return css`
@@ -21,13 +23,76 @@ export class AnypointListbox extends AnypointMenuMixin(LitElement) {
     return html`<slot></slot>`;
   }
 
+  constructor() {
+    super();
+    this.useAriaSelected = true;
+    this._selectHandler = this._selectHandler.bind(this);
+    this._deselectHandler = this._deselectHandler.bind(this);
+  }
+
   connectedCallback() {
     if (!this.hasAttribute('role')) {
       this.setAttribute('role', 'listbox');
     }
+    this.setAttribute('aria-activedescendant', '');
     /* istanbul ignore else */
     if (super.connectedCallback) {
       super.connectedCallback();
+    }
+    this.addEventListener('select', this._selectHandler);
+    this.addEventListener('deselect', this._deselectHandler);
+
+    this._initSeelction();
+  }
+
+  disconnectedCallback() {
+    /* istanbul ignore else */
+    if (super.disconnectedCallback) {
+      super.disconnectedCallback();
+    }
+    this.removeEventListener('select', this._selectHandler);
+    this.removeEventListener('deselect', this._deselectHandler);
+  }
+  /**
+   * Initializes `aria-activedescendant` when element is attached to the DOM.
+   */
+  _initSeelction() {
+    if (this.selectedItem) {
+      this._setActiveDescendant(this.selectedItem);
+    }
+  }
+  /**
+   * Sets `aria-activedescendant` value to selected element's id.
+   * @param {CustomEvent} e
+   */
+  _selectHandler(e) {
+    const { item } = e.detail;
+    this._setActiveDescendant(item);
+  }
+  /**
+   * Sets `aria-activedescendant` value to node's id.
+   * @param {Element} node
+   */
+  _setActiveDescendant(node) {
+    this._ensureNodeId(node);
+    this.setAttribute('aria-activedescendant', node.id);
+  }
+  /**
+   * Removes `aria-activedescendant` from the element when item is
+   * deselected.
+   */
+  _deselectHandler() {
+    this.setAttribute('aria-activedescendant', '');
+  }
+  /**
+   * Ensures the node to have an ID.
+   * It is later used with aria attributes.
+   * @param {Element} node
+   */
+  _ensureNodeId(node) {
+    if (!node.id) {
+      node.id = 'anypointlistbox-' + globalId;
+      globalId++;
     }
   }
 }
--- test/anypoint-listbox.test.js
+++ test/anypoint-listbox.test.js
@@ -1,5 +1,5 @@
 import { fixture, assert, nextFrame } from '@open-wc/testing';
-
+import sinon from 'sinon/pkg/sinon-esm.js';
 import '../anypoint-listbox.js';
 
 describe('AnypointListbox', () => {
@@ -12,16 +12,100 @@ describe('AnypointListbox', () => {
     </anypoint-listbox>`);
   }
 
+  async function selectedFixture() {
+    return await fixture(`<anypoint-listbox aria-label="Select one of the options" selected="1">
+      <div role="option">Item 1</div>
+      <div role="option">Item 2</div>
+      <div role="option">Item 3</div>
+      <div role="option">Item 4</div>
+    </anypoint-listbox>`);
+  }
+
   async function roleFixture() {
     return await fixture(`<anypoint-listbox role="menu"></anypoint-listbox>`);
   }
+  // This test to be run first as this element sets a global variable
+  // (in scope of the element) and increases value each time this function is used.
+  describe('_ensureNodeId()', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture();
+    });
 
-  it('has selection class name when selected', async () => {
-    const element = await basicFixture();
-    element.select(1);
-    const node = element.selectedItem;
-    assert.ok(node, 'has selected node');
-    assert.isTrue(node.classList.contains('selected'), 'has selected class');
+    it('adds id to a node if missing', () => {
+      const node = document.createElement('span');
+      element._ensureNodeId(node);
+      assert.equal(node.id, 'anypointlistbox-1');
+    });
+
+    it('increments id counter', () => {
+      const node = document.createElement('span');
+      element._ensureNodeId(node);
+      assert.equal(node.id, 'anypointlistbox-2');
+    });
+
+    it('respects existing id', () => {
+      const node = document.createElement('span');
+      node.id = 'my-id';
+      element._ensureNodeId(node);
+      assert.equal(node.id, 'my-id');
+    });
+  });
+
+  describe('Basics', () => {
+    it('has selection class name when selected', async () => {
+      const element = await basicFixture();
+      element.select(1);
+      const node = element.selectedItem;
+      assert.ok(node, 'has selected node');
+      assert.isTrue(node.classList.contains('selected'), 'has selected class');
+    });
+
+    it('has default aria-activedescendant', async () => {
+      const element = await basicFixture();
+      assert.equal(element.getAttribute('aria-activedescendant'), '');
+    });
+  });
+
+  describe('_initSeelction()', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture();
+    });
+
+    it('calls _setActiveDescendant() when has selection', () => {
+      element.selected = 1;
+      const spy = sinon.spy(element, '_setActiveDescendant');
+      element._initSeelction();
+      const node = element.querySelectorAll('div')[1];
+      assert.equal(spy.args[0][0], node);
+    });
+
+    it('ignores call to _setActiveDescendant() when no selection', () => {
+      const spy = sinon.spy(element, '_setActiveDescendant');
+      element._initSeelction();
+      assert.isFalse(spy.called);
+    });
+  });
+
+  describe('_setActiveDescendant()', () => {
+    let element;
+    beforeEach(async () => {
+      element = await basicFixture();
+    });
+
+    it('sets id on passed node', () => {
+      const node = document.createElement('span');
+      element._setActiveDescendant(node);
+      assert.include(node.id, 'anypointlistbox-');
+    });
+
+    it('sets aria-activedescendant to nodes id', () => {
+      const node = document.createElement('span');
+      node.id = 'test';
+      element._setActiveDescendant(node);
+      assert.equal(element.getAttribute('aria-activedescendant'), 'test');
+    });
   });
 
   describe('a11y', () => {
@@ -45,6 +129,37 @@ describe('AnypointListbox', () => {
       element.select(1);
       await nextFrame();
       await assert.isAccessible(element);
+    });
+
+    it('is accessible when pre selected', async () => {
+      const element = await selectedFixture();
+      await assert.isAccessible(element);
+    });
+
+    it('sets aria-activedescendant when selecting an item', async () => {
+      const element = await basicFixture();
+      element.select(2);
+      const node = element.selectedItem;
+      const des = element.getAttribute('aria-activedescendant');
+      assert.notEmpty(des, 'aria-activedescendant is set');
+      assert.equal(des, node.id);
+    });
+
+    it('removes aria-activedescendant when nothing selected', async () => {
+      const element = await basicFixture();
+      element.select(2);
+      await nextFrame();
+      element.selected = -1;
+      const des = element.getAttribute('aria-activedescendant');
+      assert.equal(des, '');
+    });
+
+    it('has aria-activedescendant when pre selected', async () => {
+      const element = await selectedFixture();
+      const node = element.selectedItem;
+      const des = element.getAttribute('aria-activedescendant');
+      assert.notEmpty(des, 'aria-activedescendant is set');
+      assert.equal(des, node.id);
     });
   });
 });
